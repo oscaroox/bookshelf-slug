4f48a6aa5127d9926a81e20d7ece3b003fd76129
oscar <oscar_rank_46@hotmail.com>
2016-08-20 00:40:18 +0200

    Adde bluebird and updated tests

--- test/database/models/Post.js
+++ test/database/models/Post.js
@@ -1,7 +1,11 @@
 'use strict';
 let bookshelf = require('../').bookshelf;
+let User = require('./User');
 
 module.exports = bookshelf.Model.extend({
   tableName: 'post',
-  slug: ['title', 'description']
+  slug: ['title', 'description'],
+  user: function() {
+    this.belgongsTo(User);
+  }
 });
--- test/database/models/User.js
+++ test/database/models/User.js
@@ -1,10 +1,13 @@
 'use strict';
 let bookshelf = require('../').bookshelf;
-
+let Post = require('./Post');
 module.exports = bookshelf.Model.extend({
   tableName: 'user',
   slug: {
     column: 'uniqueName',
     items: ['firstName', 'lastName', 'nickName']
+  },
+  post: function() {
+    return this.hasMany(Post);
   }
 });
