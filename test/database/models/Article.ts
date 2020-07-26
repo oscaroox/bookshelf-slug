183eddeabc0e5c416ce5072255de0c5691f7be62
oscar <druiventakoscar@gmail.com>
2020-07-26 13:26:24 +0200

    remove unused files

--- test/database/models/Article.ts
+++
@@ -1,7 +0,0 @@
-import { bookshelf } from "../";
-
-export default bookshelf.model("Article", {
-  tableName: "articles",
-  requireFetch: false,
-  slug: ["title"],
-});
--- test/database/models/index.ts
+++ test/database/models/index.ts
@@ -1,4 +1,2 @@
-"use strict";
-export { default as Article } from "./Article";
 export { default as Post } from "./Post";
 export { default as User } from "./User";
